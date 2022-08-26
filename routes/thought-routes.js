05466fdfdbebbe15b275ebad0df4ef7ee8d6eb31
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-08-26 12:44:55 -0500

    fixed some bugs server connects successfully now

--- routes/thought-routes.js
+++ routes/thought-routes.js
@@ -7,7 +7,7 @@ const {
   removeThought,
   addReaction,
   removeReaction
-} = require('../../controllers/thought-controller');
+} = require('../controllers/thought-controller');
 
 // /api/thoughts
 router
